cea15a9e72ff5e94de2b1c40b0e46863e365c5b3
celetor <kaiwennasi@126.com>
2022-05-01 11:59:01 +0800

    add base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   title: "Clash for Windows",
   description: "by Fndroid",
+  base: "/vuepress-doc/",
   plugins: [
     [
       "@vuepress/google-analytics",
